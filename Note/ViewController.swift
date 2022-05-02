b3ab94d260710d18d61b9d4c016af3648c69fa07
Dmitriy <94037443+ostermand@users.noreply.github.com>
2022-05-02 18:05:03 +0300

    Initial Commit

---
+++ Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by Дмитрий Остерман on 02.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
